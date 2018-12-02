9f0025e03d28701b78ab57ebc6d707cdbd76b8cb
Fabian <fabian@raabonline.eu>
2018-12-02 14:39:48 +0100

    Added Camera Source

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import CameraSource from './CameraSource';
 import './App.css';
 
 class App extends Component {
---
+++ src/CameraSource.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+
+export default class CameraSource extends Component {
+  captureImage() {
+    const context = this.canvas.getContext("2d");
+    context.drawImage(this.videoStream, 0, 0, 800, 600);
+    // const image = this.canvas.toDataURL("image/jpeg", 0.5);
+    // return image;
+  }
+
+  render() {
+      this.captureImage();
+    return (
+      <div>
+        <video
+          ref={stream => {
+            this.videoStream = stream;
+          }}
+          width="800"
+          height="600"
+          style={{ display: "none" }}
+        />
+        <canvas
+          ref={canvas => {
+            this.canvas = canvas;
+          }}
+          width="800"
+          height="600"
+          style={{ width: 800, height: 600}}
+        />
+      </div>
+    );
+  }
+}
+
+/**
+ * captureImage() {    
+  const context = this.canvas.getContext("2d")
+  context.drawImage(this.videoStream, 0, 0, 800, 600)
+  const image = this.canvas.toDataURL('image/jpeg', 0.5)
+  return image
+}
+render() {
+  return (
+   <div>
+    <video 
+      ref={(stream) => { this.videoStream = stream }}
+      width='800'
+      height='600'
+      style={{display: 'none'}}>
+    </video>
+    <canvas
+      ref={(canvas) => { this.canvas = canvas }}
+      width='800'
+      height='600'
+      style={{display: 'none'}}
+    </canvas>
+  </div>
+ )
+}
+ */

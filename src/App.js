db1d92cf88060a57768cd88d4f7b377eaf0f6153
Dominique Dorscheid <dominique.d@live.de>
2018-12-02 17:32:26 +0100

    wip 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,43 @@ import "./App.css";
 import * as posenet from "@tensorflow-models/posenet";
 
 class App extends Component {
+  net = null;
+  videoRef = null;
+
+  constructor() {
+    super();
+    this.state = {
+      modelLoaded: false,
+      refLoaded: false
+    };
+  }
+
+  refGetter(ref) {
+    this.videoRef = ref;
+    this.SetState({ refLoaded: true });
+  }
+
+  componentWillMount() {
+    posenet.load().then(net => {
+      // posenet model loaded
+      console.log("Model loaded");
+      this.SetState({ modelLoaded: true });
+      this.net = net;
+    });
+  }
+
   render() {
+    if (this.net) {
+      console.log("Hier this.net");
+      if (this.videoRef) {
+        console.log("Hier videoRef");
+        this.net
+          .estimateSinglePose(this.videoRef, 0.5, false, 16)
+          .then(pose => {
+            console.log(pose);
+          });
+      }
+    }
     return (
       <div className="App">
         <header className="App-header">
@@ -15,7 +51,7 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
         </header>
-        <Webcam audio={false} />
+        <Webcam audio={false} senselessFunction={this.refGetter.bind(this)} />
       </div>
     );
   }

13c0dce9a0ab7994f048c765728135c74b7a07b6
Fabian <fabian@raabonline.eu>
2018-12-02 14:41:38 +0100

    more camera

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import CameraSource from './CameraSource';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import CameraSource from "./CameraSource";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -12,14 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <CameraSource />
         </header>
       </div>
     );

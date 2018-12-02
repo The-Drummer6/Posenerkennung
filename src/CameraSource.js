a742fc3184ce2ee70de335e045faad6c9a0f0693
Fabian <fabian@raabonline.eu>
2018-12-02 14:53:23 +0100

    added Video LiveFeed

--- src/CameraSource.js
+++ src/CameraSource.js
@@ -1,24 +1,42 @@
 import React, { Component } from "react";
 
 export default class CameraSource extends Component {
+  
+  componentDidMount() {
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({video: true}, this.handleVideo, this.videoError);
+    } else {
+      console.error("Kacke, Funktion nicht verfügbar in dem Browser");
+    }
+  }
+
+  handleVideo(stream) {
+    // Update the state, triggering the component to re-render with the correct stream
+    this.setState({ videoSrc: window.URL.createObjectURL(stream) });
+  }
+  
   captureImage() {
-    const context = this.canvas.getContext("2d");
-    context.drawImage(this.videoStream, 0, 0, 800, 600);
+    if (this.canvas !== undefined) {
+      context = this.canvas.getContext("2d");
+      context.drawImage(this.videoStream, 0, 0, 800, 600);
+    }
     // const image = this.canvas.toDataURL("image/jpeg", 0.5);
     // return image;
   }
 
   render() {
-      this.captureImage();
+    // this.captureImage();
     return (
       <div>
         <video
           ref={stream => {
             this.videoStream = stream;
           }}
+          src={this.state.videoSrc} 
+          autoPlay="true"
           width="800"
           height="600"
-          style={{ display: "none" }}
         />
         <canvas
           ref={canvas => {
@@ -26,7 +44,7 @@ export default class CameraSource extends Component {
           }}
           width="800"
           height="600"
-          style={{ width: 800, height: 600}}
+          style={{ width: 800, height: 600 }}
         />
       </div>
     );
@@ -59,3 +77,29 @@ render() {
  )
 }
  */
+
+/**
+var MyComponent = React.createClass({
+    getInitialState: function(){
+      return { videoSrc: null }
+    },
+    componentDidMount: function(){
+      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+      if (navigator.getUserMedia) {
+          navigator.getUserMedia({video: true}, this.handleVideo, this.videoError);
+      }
+    },
+    handleVideo: function(stream) {
+      // Update the state, triggering the component to re-render with the correct stream
+      this.setState({ videoSrc: window.URL.createObjectURL(stream) });
+    },
+    videoError: function() {
+  
+    },
+    render: function() {
+      return <div>
+        <video src={this.state.videoSrc} autoPlay="true" />
+      </div>;
+      }
+  });
+*/

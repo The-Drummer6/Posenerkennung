d47d53c2f2e85158744c67da973a4850b78d8c52
Fabian <fabian@raabonline.eu>
2018-12-03 14:03:28 +0100

    Import klein gemacht

--- src/Webcam.js
+++ src/Webcam.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import * as posenet from "@tensorflow-models/posenet";
-import { loadModelFromIOHandler } from "@tensorflow/tfjs-layers/dist/models";
 import {
   drawBoundingBox,
   drawKeypoints,
